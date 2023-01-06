bebb519591f3c1626f1b66c3533503064bb132f8
f-evgeniya <f.evgeniya.ef@gmail.com>
2023-01-06 11:22:27 +0200

    Imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import "./App.css";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
-    <div className="App container">
-      <Weather />
-      <p>
-        Created by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/54185-evgeniya-firsova"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Evgeniya Firsova
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/f-evgeniya/shecodes-react-weather"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://app.netlify.com/sites/delightful-lebkuchen-7d8bbd/deploys"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </p>
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <p>
+          Created by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/54185-evgeniya-firsova"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Evgeniya Firsova
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/f-evgeniya/shecodes-react-weather"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://app.netlify.com/sites/delightful-lebkuchen-7d8bbd/deploys"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
