f710ca7054c09c6734e8d6579952e6d48b132d44
f-evgeniya <f.evgeniya.ef@gmail.com>
2023-01-15 17:39:10 +0200

    Fixed 5-day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./weatherForecast.css";
-
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.data.city]);
+
   function handleResponse(response) {
     console.log(response.data);
     setForecast(response.data.daily);
